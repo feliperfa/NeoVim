24c28670b745525a4953770bab301a435e56f7f8
Felipe Almeida <felipealmeida@MacBook-Pro.local>
2025-06-19 14:15:02 +0100

    Keeping the show references generic from the lsp instead of omnisharp extended

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -15,6 +15,7 @@ M.on_attach = function(_, bufnr, client)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, opts "List workspace folders")
 
+  map("n", "gr", vim.lsp.buf.references, opts "Show references")
   map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
   map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
   map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
@@ -22,12 +23,12 @@ M.on_attach = function(_, bufnr, client)
   if client.name == "omnisharp" then
     map("n", "gd", require("omnisharp_extended").lsp_definition, opts "Go to definition")
     map("n", "gD", require("omnisharp_extended").lsp_type_definition, opts "Go to declaration")
-    map("n", "gr", require("omnisharp_extended").lsp_references, opts "Show references")
+    -- map("n", "gr", require("omnisharp_extended").lsp_references, opts "Show references")
     map("n", "gi", require("omnisharp_extended").lsp_implementation, opts "Go to implementation")
   elseif client.name ~= "GitHub Copilot" then
     map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
     map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
-    map("n", "gr", vim.lsp.buf.references, opts "Show references")
+    -- map("n", "gr", vim.lsp.buf.references, opts "Show references")
     map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
   end
 end
